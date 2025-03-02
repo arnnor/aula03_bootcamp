f2859c759b4ebb600f506f4779b89f0817905695
arnnor rocha <arnnor.rocha@gmail.com>
2025-03-02 18:13:28 -0300

    Estudando estruturas de controle de fluxo.

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 # estudando estruturas de controle de fluxo if, for e while
 
-x = int(input("Please enter an integer: "))
+x = False
 
-if x < 0:
-    x = 0
-    print('Negative changed to zero')
-elif x == 0:
-    print('Zero')
-elif x == 1:
-    print('Single')
-else:
-    print('More')
+while not x:
+    try:
+        x = int(input("Digite um número inteiro: "))
+        if x < 0:
+            x = 0
+            print('Número negativo.')
+        elif x == 0:
+            print('Zero.')
+        elif x == 1:
+            print('Um.')
+        else:
+            print('Mais que um.')
+    except ValueError:
+        print("Digite um valor válido.")
+    
